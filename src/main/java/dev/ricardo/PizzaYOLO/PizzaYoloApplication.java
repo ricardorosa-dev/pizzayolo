b29472c2ab7a850391a16e2f44e8a5522e2f87ec
ricardorosa-dev <ricardorosa@gmail.com>
2021-11-22 12:23:10 -0300

    Iniciando projeto PizzaYOLO

---
+++ src/main/java/dev/ricardo/PizzaYOLO/PizzaYoloApplication.java
@@ -0,0 +1,13 @@
+package dev.ricardo.PizzaYOLO;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzaYoloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzaYoloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/ricardo/PizzaYOLO/PizzaYoloApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ricardo.PizzaYOLO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzaYoloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
